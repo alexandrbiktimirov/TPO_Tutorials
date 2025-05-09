20345da8c9407a28fb367c041273851e2065764d
s30175 <alex.biktimirov38@gmail.com>
2025-05-09 17:05:25 +0200

    Initial commit

---
+++ tutorial9/src/main/java/org/example/tutorial9/Tutorial9Application.java
@@ -0,0 +1,13 @@
+package org.example.tutorial9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorial9Application.class, args);
+    }
+
+}
---
+++ tutorial9/src/test/java/org/example/tutorial9/Tutorial9ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tutorial9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
